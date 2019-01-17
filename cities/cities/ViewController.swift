5243bb77d6e872f8f5390e0599cd3fe7452d2898
Martin Roeder <roederm@gmail.com>
2019-01-16 20:17:27 -0600

    Create project

---
+++ cities/cities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cities
+//
+//  Created by Martin Roeder on 1/16/19.
+//  Copyright © 2019 Martin Roeder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
